d22a843231ddc8b90fff7198d86a2f45c0127f40
Bobrov <psdzofficial@yandex.ru>
2020-09-30 11:32:39 +0300

    viewed games from https://api.rawg.io/api/games

--- client/components/settings/AddGame.vue
+++ client/components/settings/AddGame.vue
@@ -1,34 +1,37 @@
 <template>
-<div>
+<div class="card">
             <div class="card">
   <div class="card-header">
     Добавить игру в базу
   </div>
   <div class="card-body text-center">
       <form @submit.prevent="">
-          <input v-model="name" class="form-control mb-4" type="text" placeholder="Search" aria-label="Search">
+          <input v-model="search" class="form-control mb-4" type="text" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-primary w-50">add game</button>
       </form>
   </div>
   <hr>
   
-  <div class="card-deck">
+  <div class="row">
+      <!--<div class="col-lg-6">-->
+          <!--&lt;!&ndash;<div class="card">&ndash;&gt;-->
+          <!--&lt;!&ndash;<img src="https://images-na.ssl-images-amazon.com/images/I/71nlEoSrewL._AC_SL1200_.jpg" class="rounded-top card-img-top" alt="">&ndash;&gt;-->
+          <!--&lt;!&ndash;<div class="card-body">&ndash;&gt;-->
+          <!--&lt;!&ndash;<h5 class="card-tittle text-center">Card Title</h5>&ndash;&gt;-->
+          <!--&lt;!&ndash;<p class="card-text">Date release</p>&ndash;&gt;-->
+          <!--&lt;!&ndash;</div>&ndash;&gt;-->
+          <!--&lt;!&ndash;</div>&ndash;&gt;-->
+      <!--</div>-->
+      <div v-for="game in api" class="col-lg-6">
           <div class="card">
-              <img h src="https://images-na.ssl-images-amazon.com/images/I/71nlEoSrewL._AC_SL1200_.jpg" class="rounded-top card-img-top" alt="">
+              <img :src="game['background_image']" class="rounded card-img-top" alt="">
               <div class="card-body">
-                  <h5 class="card-tittle text-center">Card Title</h5>
-                  <p class="card-text">Date release</p>
-              </div>
-          </div>
-          <div class="card">
-              <img :src="api['image_background']" class="rounded-top card-img-top" alt="">
-              <div class="card-body">
-                  <h5 class="card-tittle text-center">Card Title</h5>
-                  <p class="card-text">Date release</p>
-                  <p class="card-text"></p>
+                  <h5 class="card-tittle text-center">{{game.name}}</h5>
+                  <p class="card-text">{{game.released}}</p>
               </div>
           </div>
       </div>
+  </div>
 </div>
 
 </div>
@@ -37,18 +40,22 @@
 export default {
     data (context) {
         return {
-            name : '',
-            games : null,
             api : [],
-            img : []
-
+            search : ''
         }
     },
     async mounted() {
-        const response = await fetch ('https://api.rawg.io/api/developers?search=Electronic%20Arts&page_size=1').then(res => res.json());
-        this.api = response.results[0]
+        console.log('https://api.rawg.io/api/games');
+        const response = await fetch ('https://api.rawg.io/api/games').then(res => res.json());
+        this.api = response.results;
+        console.log(this.api);
+    },
+    computed : {
+        filteredGames : function () {
+            return this.api.filter((game) => {
+                return boolean ;
+            })
+        }
     }
-
-    
 }
 </script>
